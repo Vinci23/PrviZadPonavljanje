0f844b0e9d2c8d25c39cafec1f89ce5e837f326c
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-02-01 13:00:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrviZadPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Upišite stranice trokuta(x, y, z): ");
+            Console.WriteLine("Upišite x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upišite y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upišite z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            
+            if(x>0 && y>0 && z>0)
+            {
+                if ((x * x) + (y * y) == (z * z))
+                {
+                    Console.WriteLine("Trokut je pravokutan");
+                }
+                else
+                {
+                    Console.WriteLine("trokut nije pravokutan");
+                }
+
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
